7e7b719e3df9e0c1ca3145bc050859dd19e27b10
Emerson Sousa <eyenessus@gmail.com>
2025-08-24 13:10:52 -0300

    feature: create controller api rest

---
+++ src/main/java/com/whaply/Groccy/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.whaply.Groccy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.whaply.Groccy.infrastructure.entitys.User;
+import com.whaply.Groccy.interfaces.IUserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/")
+    public User createUser(@RequestBody User user) {
+        return userService.create(user);
+    }
+}
