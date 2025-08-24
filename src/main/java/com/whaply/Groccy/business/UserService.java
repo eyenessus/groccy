8b18d3c14353a74b73df60129562ac2427b70a91
Emerson Sousa <eyenessus@gmail.com>
2025-08-24 13:11:28 -0300

    feature: create business logic

---
+++ src/main/java/com/whaply/Groccy/business/UserService.java
@@ -0,0 +1,35 @@
+package com.whaply.Groccy.business;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.whaply.Groccy.infrastructure.entitys.User;
+import com.whaply.Groccy.infrastructure.repository.UserRepository;
+import com.whaply.Groccy.interfaces.IUserService;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class UserService implements  IUserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() -> new EntityNotFoundException("User not Found"));
+    }
+
+    public void deleteByUsername(String username) {
+        userRepository.deleteByUsername(username);
+    }
+    
+    public User update(User user){
+        User existingUser = userRepository.findByUsername(user.getUsername()).orElseThrow(() -> new EntityNotFoundException("User not Found"));
+        if(user.getName() != null) existingUser.setName(user.getName());
+        if(user.getPassword() != null) existingUser.setPassword(user.getPassword());
+        return userRepository.save(existingUser);
+    }
+}
