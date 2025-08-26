fd9eab6e492ef276d24de4c8eee48ef0b03e5da2
Emerson Sousa <eyenessus@gmail.com>
2025-08-26 01:40:22 -0300

    fix: table name to users

--- src/main/java/com/whaply/Groccy/infrastructure/entities/User.java
+++ src/main/java/com/whaply/Groccy/infrastructure/entities/User.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
